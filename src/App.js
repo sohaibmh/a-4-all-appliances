713970c3a008a56e2c82c4e25c77daa3e6460f56
Sohaib Mubashir <sohaibmh@yahoo.com>
2020-12-09 09:49:50 +0000

    added contact details

--- src/App.js
+++ src/App.js
@@ -22,7 +22,8 @@ function App() {
       </div>
 
       <div
-        className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center "
+        className="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center"
+        id="mainSection"
         style={{ backgroundColor: "#F7F7F7" }}
       >
         <div className="col-md-5 p-lg-5 mx-auto my-5">
@@ -164,137 +165,25 @@ function App() {
       </div>
 
       <footer className="container py-5">
-        <div className="row">
+        <div className="row text-muted" id={"footer"}>
           <div className="col-12 col-md">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              fill="none"
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              className="d-block mb-2"
-              role="img"
-              viewBox="0 0 24 24"
-              focusable="false"
-            >
-              <title>Product</title>
-              <circle cx="12" cy="12" r="10" />
-              <path d="M14.31 8l5.74 9.94M9.69 8h11.48M7.38 12l5.74-9.94M9.69 16L3.95 6.06M14.31 16H2.83m13.79-4l-5.74 9.94" />
-            </svg>
-            <small className="d-block mb-3 text-muted">&copy; 2017-2020</small>
+            <img src={logo} className="footerLogo" />
           </div>
-          <div className="col-6 col-md">
-            <h5>Features</h5>
-            <ul className="list-unstylesd text-small">
-              <li>
-                <a className="text-muted" href="#">
-                  Cool stuff
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Random feature
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Team feature
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Stuff for developers
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Another one
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Last time
-                </a>
-              </li>
-            </ul>
+          <div>
+            <h5>Our Address</h5>
+            East Lane Business Park <br />
+            Office 11 Silver Box House <br />
+            56 Magnet Road <br />
+            Wembley
+            <br />
+            HA9 7RG
+            <br />
           </div>
-          <div className="col-6 col-md">
-            <h5>Resources</h5>
-            <ul className="list-unstylesd text-small">
-              <li>
-                <a className="text-muted" href="#">
-                  Resource
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Resource name
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Another resource
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Final resource
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-6 col-md">
-            <h5>Resources</h5>
-            <ul className="list-unstylesd text-small">
-              <li>
-                <a className="text-muted" href="#">
-                  Business
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Education
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Government
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Gaming
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="col-6 col-md">
-            <h5>About</h5>
-            <ul className="list-unstylesd text-small">
-              <li>
-                <a className="text-muted" href="#">
-                  Team
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Locations
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Privacy
-                </a>
-              </li>
-              <li>
-                <a className="text-muted" href="#">
-                  Terms
-                </a>
-              </li>
-            </ul>
+          <div>
+            <h5>Contact Us</h5>
+            020 7998 6900 <br />
+            079 5848 0832 <br />
+            info@a4allappliances.com
           </div>
         </div>
       </footer>
